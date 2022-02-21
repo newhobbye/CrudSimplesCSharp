e30896ef264f6cd2f866d479ddbc4688d1ce134d
Robson <robsonhobbye@gmail.com>
2022-02-21 15:46:09 -0300

    Projeto simples para consulta

---
+++ FuncionariosAPI/Controllers/FuncionarioController.cs
@@ -0,0 +1,65 @@
+﻿using FuncionariosAPI.Data;
+using FuncionariosAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FuncionariosAPI.Controllers
+{
+    [ApiController]
+    [Route("/funcionario")]
+    public class FuncionarioController : ControllerBase
+    {
+        private FuncionarioContext _funcionario;
+
+        public FuncionarioController(FuncionarioContext funcionario)
+        {
+            _funcionario = funcionario;
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarFuncionario([FromBody] Funcionario funcionario)
+        {
+            if (funcionario == null)
+            {
+                return NotFound();
+            }
+
+            _funcionario.Funcionarios.Add(funcionario);
+            _funcionario.SaveChanges();
+            return CreatedAtAction(nameof(RecuperarFuncionario), new { CPF = funcionario.CPF }, funcionario);
+        }
+
+        [HttpGet]
+        public IEnumerable<Funcionario> RecuperarFuncionario()
+        {
+            return _funcionario.Funcionarios;
+        }
+
+        [HttpGet("{cpf}")]
+        public IActionResult RecuperarFuncionariosPorCPF(string cpf)
+        {
+            Funcionario funcionario = _funcionario.Funcionarios.FirstOrDefault(x => x.CPF == cpf);
+            if(funcionario != null)
+            {
+                return Ok(funcionario);
+            }
+
+            return NotFound();
+        }
+
+        [HttpDelete("{cpf}")]
+        public IActionResult DeletarFuncionariosPorCPF(string cpf)
+        {
+            Funcionario funcionario = _funcionario.Funcionarios.FirstOrDefault(x => x.CPF == cpf);
+            if (funcionario != null)
+            {
+                _funcionario.Remove(funcionario);
+                _funcionario.SaveChanges();
+                return NoContent();
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ FuncionariosAPI/Migrations/20220221162952_CriandoTabelaFuncionarios.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace FuncionariosAPI.Migrations
+{
+    public partial class CriandoTabelaFuncionarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Funcionarios",
+                columns: table => new
+                {
+                    CPF = table.Column<string>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "datetime", nullable: false),
+                    Company = table.Column<string>(type: "text", nullable: false),
+                    Position = table.Column<string>(type: "text", nullable: false),
+                    Salary = table.Column<double>(type: "double", nullable: false),
+                    State = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    District = table.Column<string>(type: "text", nullable: false),
+                    Street = table.Column<string>(type: "text", nullable: false),
+                    NumberOfStreet = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_funcionarios", x => x.CPF);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "funcionarios");
+        }
+    }
+}
